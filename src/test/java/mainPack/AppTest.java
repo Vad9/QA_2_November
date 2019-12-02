cf2e7daca2d364e33821531d59220b7c925d47b5
vadim.korobchuk <vadim.korobchuk@developex.net>
2019-12-02 19:05:11 +0200

    test 1

---
+++ src/test/java/mainPack/AppTest.java
@@ -0,0 +1,7 @@
+package mainPack;
+
+public class AppTest {
+    //good
+    //good2222
+
+}
---
+++ src/test/java/parentTest/ParentTest.java
@@ -0,0 +1,36 @@
+package parentTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+
+// класс родитель для всех Тестов, все общьее выносим сюда
+public class ParentTest {
+    WebDriver webDriver;
+    protected LoginPage loginPage;
+    protected HomePage homePage;
+
+    @Before
+    public void setUp() {
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        webDriver = new ChromeDriver();
+//      webDriver = new FirefoxDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        loginPage = new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+}
